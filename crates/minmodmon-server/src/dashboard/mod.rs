30a02aa1d6e990e517ddc837a45d0b0a8b7876e3
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2024-05-23 12:03:54 +0200

    Add templating

--- crates/minmodmon-server/src/dashboard/mod.rs
+++ crates/minmodmon-server/src/dashboard/mod.rs
@@ -1,5 +1,7 @@
 use anyhow::Error;
 use salvo::{handler, writing::Text, Depot, Response};
+use serde::Serialize;
+use tinytemplate::TinyTemplate;
 
 use crate::agent::agent_service;
 
@@ -8,12 +10,25 @@ pub async fn handle(depot: &mut Depot, res: &mut Response) -> Result<(), Error> 
     let service = agent_service(depot)?;
     let manager = service.manager().await;
 
+    // Prepare template
     let template = include_str!("dashboard.html");
+    let mut tt = TinyTemplate::new();
+    tt.add_template("dashboard", template)?;
+
+    // Prepare context data
     let info = manager.model_info();
+    let context = Context {
+        loaded_model: info.id.clone(),
+    };
 
-    let rendered = template.replace("$LOADED_MODEL", &info.id);
-
+    // Render and reply with the template
+    let rendered = tt.render("dashboard", &context)?;
     res.render(Text::Html(rendered));
 
     Ok(())
+}
+
+#[derive(Serialize)]
+struct Context {
+    loaded_model: String,
 }
